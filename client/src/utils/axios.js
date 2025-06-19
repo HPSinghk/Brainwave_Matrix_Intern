c8af26b0c17cff5a10b14a301f68bc60a8b52a5b
Harendra Pratap Singh <contact.harendra.singh@gmail.com>
2025-06-19 11:45:15 +0530

    again fixing env variables

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  
+  baseURL: `${process.env.REACT_APP_API_URL}/api`,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
