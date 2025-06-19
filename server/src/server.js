424332a0bb38159ed45d7b8c7d570ee8e9bb9adc
Harendra Pratap Singh <contact.harendra.singh@gmail.com>
2025-06-19 12:07:59 +0530

    fixing backend env

--- server/src/server.js
+++ server/src/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: `${process.env.CLIENT_URL}`,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
