a3a2ad05d9309b56f438551cafa49dd72a366c7f
Harendra Pratap Singh <contact.harendra.singh@gmail.com>
2025-06-20 22:55:42 +0530

    fixing register error

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -40,7 +40,7 @@ const Register = () => {
     setError('');
     
     try {
-      const res = await axios.post('http://localhost:8000/api/auth/register', {
+      const res = await axios.post(`${process.env.REACT_APP_API_URL}api/auth/register`, {
         name,
         email,
         password
